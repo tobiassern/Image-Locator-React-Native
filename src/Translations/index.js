ad0aeb99b8cbeb4c9f3f2d94a86f60d3dd2a8d42
Tobias Sernhede <tobiassernhede@Tobiass-MacBook-Pro.local>
2017-08-17 16:51:39 +0200

    updated the logic of translation function to manage different regions for a language

--- src/Translations/index.js
+++ src/Translations/index.js
@@ -5,9 +5,18 @@ import { sv } from './sv';
 const locale = null;
 
 Util.getCurrentLocaleAsync().then((response) => { 
-	locale = response;
+	console.log("locale: ", response);
+	let short_locale = response.split('-');
+	console.log(short_locale);
+	short_locale = short_locale[0];
+	console.log(short_locale);
+	locale = short_locale;
 });
 const translations = { en, sv }
 export function translate(string) {
-	return translations[locale] && translations[locale][string] ? translations[locale][string] : '** No translation **';
+	if(translations[locale]) {
+		return translations[locale][string] ? translations[locale][string] : '** No translation **';
+	} else {
+		return translations.en[string] ? translations.en[string] : '** No translation **';
+	}
 }
