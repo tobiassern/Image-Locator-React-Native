3a882af87e317c913d146dedb3ad550ebb064234
Tobias Sernhede <tobiassernhede@Tobiass-MacBook-Pro.local>
2017-08-17 17:45:43 +0200

    first part of panResponder animation on modal

--- src/Components/MarkerInfoModal/index.js
+++ src/Components/MarkerInfoModal/index.js
@@ -1,6 +1,6 @@
 /**--- Node Modules ---**/
 import React, { Component } from 'react';
-import { StyleSheet, View, Modal, Image, TouchableOpacity, Dimensions, Animated, Easing } from 'react-native';
+import { StyleSheet, View, Modal, Image, TouchableOpacity, Dimensions, Animated, Easing, PanResponder } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 /**--- Core ---**/
@@ -17,25 +17,75 @@ import Text from 'App/Components/Text';
 const deviceWidth = Dimensions.get('window').width;
 const deviceHeight = Dimensions.get('window').height;
 
+// https://mindthecode.com/getting-started-with-the-panresponder-in-react-native/
+
 export default class MarkerInfoModal extends Component {
 
 	constructor(props) {
 		super(props);
-		this.animateImage = new Animated.Value(0);
-		this.animateInfo = new Animated.Value(0);
+		this.animateModal = new Animated.Value(0);
 		this.imageInitialWidth = '100%';
 		this.imageInitialHeight = '100%';
+		this.state = {
+			touching: false
+		}
+		this._setState = function(state) {
+			this.setState(state);
+		}
+	}
+
+	componentWillMount() {
+		this._panResponder = PanResponder.create({
+		      // Ask to be the responder:
+		      onStartShouldSetPanResponder: (evt, gestureState) => true,
+		      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+		      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+		      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+
+		      onPanResponderGrant: (evt, gestureState) => {
+		        // The gesture has started. Show visual feedback so the user knows
+		        // what is happening!
+		        console.log("grant");
+		        // gestureState.d{x,y} will be set to zero now
+		      },
+		      onPanResponderMove: (evt, gestureState) => {
+		      	console.log("moving");
+		        // The most recent move distance is gestureState.move{X,Y}
+
+		        // The accumulated gesture distance since becoming responder is
+		        // gestureState.d{x,y}
+		      },
+		      onPanResponderTerminationRequest: (evt, gestureState) => true,
+		      onPanResponderRelease: (evt, gestureState) => {
+		      	console.log("release");
+		        // The user has released all touches while this view is the
+		        // responder. This typically means a gesture has succeeded
+		      },
+		      onPanResponderTerminate: (evt, gestureState) => {
+		        // Another component has become the responder, so this gesture
+		        // should be cancelled
+		      },
+		      onShouldBlockNativeResponder: (evt, gestureState) => {
+		        // Returns whether this component should block native components from becoming the JS
+		        // responder. Returns true by default. Is currently only supported on android.
+		        return true;
+		      },
+		    });
 	}
 
 	componentWillReceiveProps({modalMarker}) {
 		if(modalMarker && this.props.modalMarker !== modalMarker) {
-			this.animateImageInitiate(1);
+			this.animateModalInitiate(1);
 			this.imageInitialWidth = this.getPercentDifference(modalMarker.measure.height / modalMarker.measure.width);
 			this.imageInitialHeight = this.getPercentDifference(modalMarker.measure.width / modalMarker.measure.height);
 		}
 		if(!modalMarker) {
-			this.animateImage = new Animated.Value(0);
+			this.animateModal = new Animated.Value(0);
 		}
+	}
+
+	componentWillUnmount() {
+		this._setState = () => {};
 	}
 
 	getPercentDifference(diff) {
@@ -45,32 +95,23 @@ export default class MarkerInfoModal extends Component {
 			return '100%';
 		}
 	}
-	animateImageInitiate(value) {
+	animateModalInitiate(value) {
+		value = value === 1 ? deviceHeight / 3 : 0;
 		Animated.timing(
-			this.animateImage,
+			this.animateModal,
 			{
 				toValue: value,
-				duration: CONSTANTS.MARKER_MODAL_IMAGE_ANIMATION_DURATION,
+				duration: CONSTANTS.MARKER_MODAL_DURATION,
 				easing: Easing.ease
 			}).start();
 	}
 
-	animateInfoInitiate(value) {
-		Animated.timing(
-			this.animateInfo,
-			{
-				toValue: value,
-				duration: 0,
-				delay: CONSTANTS.MARKER_MODAL_IMAGE_ANIMATION_DURATION,
-				easing: Easing.bounce
-			}).start();
-	}
 
 	closeModal() {
-		this.animateImageInitiate(0);
+		this.animateModalInitiate(0);
 		setTimeout(() => {
 			this.props.closeModal();
-		}, CONSTANTS.MARKER_MODAL_IMAGE_ANIMATION_DURATION);
+		}, CONSTANTS.MARKER_MODAL_DURATION);
 	}
 
 	renderInnerModal() {
@@ -78,7 +119,7 @@ export default class MarkerInfoModal extends Component {
 			<Animated.View
 				style={{
 					flex: 1,
-					backgroundColor: this.animateImage.interpolate({
+					backgroundColor: this.animateModal.interpolate({
 						inputRange: [0, 1],
 						outputRange: ['transparent', COLORS.MARKER_MODAL_BACKGROUND]
 					})
@@ -92,31 +133,43 @@ export default class MarkerInfoModal extends Component {
 					<Ionicons name="ios-close-outline" size={39} color="#ffffff" />
 					</View>
 				</TouchableOpacity>
-
+				<View
+					{...this._panResponder.panHandlers}
+					style={{
+						backgroundColor: 'red',
+						width: 200,
+						height: 200,
+						position: 'absolute',
+						zIndex: 300,
+						left: 100,
+						top: 200
+					}}
+				>
+				</View>
 				<Animated.View
 					style={{
 						position: 'absolute',
 						justifyContent: 'center',
 						alignItems: 'center',
 						overflow: 'hidden',
-						width: this.animateImage.interpolate({
-							inputRange: [0, 1],
+						width: this.animateModal.interpolate({
+							inputRange: [0, deviceHeight / 3],
 							outputRange: [CONSTANTS.MARKER_CIRCLE_SIZE - CONSTANTS.MARKER_CIRCLE_PADDING, deviceWidth]
 						}),
-						height: this.animateImage.interpolate({
-							inputRange: [0, 1],
+						height: this.animateModal.interpolate({
+							inputRange: [0, deviceHeight / 3],
 							outputRange: [CONSTANTS.MARKER_CIRCLE_SIZE - CONSTANTS.MARKER_CIRCLE_PADDING, deviceHeight]
 						}),
-						borderRadius: this.animateImage.interpolate({
-							inputRange: [0, 1],
+						borderRadius: this.animateModal.interpolate({
+							inputRange: [0, deviceHeight / 3],
 							outputRange: [CONSTANTS.MARKER_CIRCLE_SIZE - CONSTANTS.MARKER_CIRCLE_PADDING, 0]
 						}),
-						left: this.animateImage.interpolate({
-							inputRange: [0, 1],
+						left: this.animateModal.interpolate({
+							inputRange: [0, deviceHeight / 3],
 							outputRange: [this.props.modalMarker.measure.px + 5, 0]
 						}),
-						top: this.animateImage.interpolate({
-							inputRange: [0, 1],
+						top: this.animateModal.interpolate({
+							inputRange: [0, deviceHeight / 3],
 							outputRange: [this.props.modalMarker.measure.py + 5, 0]
 						}),
 					}}
@@ -125,12 +178,12 @@ export default class MarkerInfoModal extends Component {
 						style={
 							{
 								position: 'absolute',
-								width: this.animateImage.interpolate({
-									inputRange: [0, 1],
+								width: this.animateModal.interpolate({
+									inputRange: [0, deviceHeight / 3],
 									outputRange: [this.imageInitialWidth, '100%']
 								}),
-								height: this.animateImage.interpolate({
-									inputRange: [0, 1],
+								height: this.animateModal.interpolate({
+									inputRange: [0, deviceHeight / 3],
 									outputRange: [this.imageInitialHeight, '100%']
 								}),
 								resizeMode: 'contain'
