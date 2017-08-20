ba4ef307c6f6039b70d906157626b568404d6881
Tobias Sernhede <tobias@sernhede.com>
2017-08-20 18:33:29 +0200

    enabled facebook ads

--- src/Components/Banner/index.js
+++ src/Components/Banner/index.js
@@ -6,10 +6,12 @@ import { FacebookAds } from 'expo';
 /**--- Core ---**/
 import { ENV } from 'App/Core/env';
 
+FacebookAds.AdSettings.addTestDevice(FacebookAds.AdSettings.currentDeviceHash);
+
 export default class Banner extends Component {
 	render() {
 		return (
-			<View>
+			<View style={{height: 70}}>
 				{ENV.FB_BANNER_PLACEMENT_ID &&
 				<FacebookAds.BannerView
 	        		placementId={ENV.FB_BANNER_PLACEMENT_ID}
